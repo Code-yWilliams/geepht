678dd5421b684becee55dbc0bd0594d757b16c71
Cody Williams <cody@codywilliams.dev>
2025-01-03 20:37:44 -0800

    set up tsconfig alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
-})
+  plugins: [RubyPlugin(), tsConfigPaths()],
+});
