4b34cdc8a3a58166819c96f8566c8caf66b5005e
Cody Williams <cody@codywilliams.dev>
2025-01-04 21:53:48 -0800

    simplify react component rendering

--- app/helpers/react_helper.rb
+++ app/helpers/react_helper.rb
@@ -1,5 +1,10 @@
 module ReactHelper
   def react_component(name, props = {})
-    content_tag(:div, nil, data: { component: name, props: props.to_json })
+    entrypoint_file_name = "#{name.underscore}.tsx"
+
+    safe_join([
+      content_tag(:div, nil, data: { component: name, props: props.to_json }),
+      vite_typescript_tag(entrypoint_file_name)
+    ])
   end
 end
